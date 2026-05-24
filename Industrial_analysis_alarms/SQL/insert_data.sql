0e98ddb347909b37ed835d98a6efbb625feec378
Dalen98 <davidleoni_98@hotmail.com>
2026-05-24 10:53:19 -0500

    Update insert_data.sql

--- Industrial_analysis_alarms/SQL/insert_data.sql
+++ Industrial_analysis_alarms/SQL/insert_data.sql
@@ -1,1 +1,11 @@
+-- insertar valores
+insert into equipos values
+(1,"bomba p-101","Molienda"),
+(2,"Motor M-202","fajas"),
+(3, "compresor c-301","planta");
 
+insert into alarmas values
+(1,1,"Alta","2026-05-20 08:30:00","sobretemperatura"),
+(2,1,"Media","2026-05-20 09:10:00","baja presion"),
+(3,2,"Alta","2026-05-20 10:00:00","sobrecorriente"),
+(4,3,"Baja","2026-05-20 11:20:00","manteniemiento");
