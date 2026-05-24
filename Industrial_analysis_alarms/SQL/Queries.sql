3364389dece3e9f4212980684fd7441b262ed759
Dalen98 <davidleoni_98@hotmail.com>
2026-05-24 10:54:15 -0500

    Update Queries.sql

--- Industrial_analysis_alarms/SQL/Queries.sql
+++ Industrial_analysis_alarms/SQL/Queries.sql
@@ -1,1 +1,26 @@
+-- ver alarmas con nombre del equipo
 
+select e.nombre, a.prioridad, a.dscripcion
+from alarmas as a
+inner join equipos as e
+	on a.equipo_id = e.id;
+
+-- equipo con mas alarmas
+
+select e.nombre, e.area, count(*) as num_alm
+from alarmas as a
+join equipos as e
+	on a.equipo_id = e.id
+group by e.nombre, e.area;    
+
+-- contar alarmas por prioridad
+select a.prioridad, count(*) as num_alm_prior
+from alarmas as a
+group by a.prioridad;
+
+-- equipo y prioridad
+select e.nombre, a.prioridad, count(*) as num_alm
+from alarmas as a
+join equipos as e
+	on a.equipo_id = e.id
+group by e.nombre, a.prioridad;   
