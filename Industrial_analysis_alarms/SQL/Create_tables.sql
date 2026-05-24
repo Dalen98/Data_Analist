ef2b1f29f9f41d769e5e517489592bea28310d70
Dalen98 <davidleoni_98@hotmail.com>
2026-05-24 10:51:46 -0500

    Update Create_tables.sql

--- Industrial_analysis_alarms/SQL/Create_tables.sql
+++ Industrial_analysis_alarms/SQL/Create_tables.sql
@@ -1,1 +1,17 @@
+-- creando bd
+create database planta_industrial;
+use planta_industrial;
+-- creando tablas
+create table equipos(
+id int primary key,
+nombre varchar(50),
+area varchar (50)
+);
 
+create table alarmas (
+id int primary key,
+equipo_id int,
+prioridad varchar(20),
+fecha datetime,
+descripcion varchar(100)
+);
